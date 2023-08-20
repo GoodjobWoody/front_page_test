bec299ed46b9d99820fa7011380d0ce03b4198e4
DoRightThing <rightwill@DoRightThings-MacBook-Pro.local>
2023-08-20 15:30:48 -0400

    show modal

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ function injectModal() {
             <span class="close">&times;</span>
             <iframe id="resumeFrame" src="${chrome.runtime.getURL(
               "resume.html"
-            )}"></iframe>
+            )}" frameborder="0"></iframe>
         </div>
     `;
 
@@ -20,18 +20,30 @@ function injectModal() {
   // Add styles for modal (you can expand this with your previous styles)
   const style = document.createElement("style");
   style.textContent = `
-        #resumeModal {
-            display: none;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0,0,0,0.7);
-            z-index: 10000;
-        }
-        /* ... (other styles) ... */
-    `;
+  #resumeModal {
+      display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0,0,0,0.7);
+      z-index: 10000;
+  }
+  #resumeModal .modal-content {
+      width: 90%;  /* Adjust this value as needed */
+      height: 80%; /* Adjust this value as needed */
+      margin: auto;
+      position: relative;
+      background: white; /* Optional: to give the modal content a background */
+      overflow: auto;    /* To handle content that might overflow */
+  }
+  #resumeModal iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
+  }
+`;
   document.head.appendChild(style);
 
   // Show modal
@@ -43,6 +55,7 @@ function injectModal() {
   });
 }
 
+// No need to fetch the content of resume.html anymore
 chrome.runtime.onMessage.addListener((message) => {
   if (message.action === "openModal") {
     injectModal();
