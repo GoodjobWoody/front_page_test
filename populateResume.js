9430753c24ee401f5033aaebbebd95bb8c221ae8
DoRightThing <rightwill@DoRightThings-MacBook-Pro.local>
2023-08-20 20:03:51 -0400

    populate data

--- populateResume.js
+++ populateResume.js
@@ -8,17 +8,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function populateData(resumeData) {
-  // Sample JSON data (you would fetch this from your JSON file in a real scenario)
-
   // Populate Personal Information
-  document.getElementById("name").innerText = resumeData.name;
-  document.getElementById("phone").innerText = resumeData.contact.phone;
-  document.getElementById("email").innerText = resumeData.contact.email;
-  document.getElementById("location").innerText = resumeData.contact.location;
-  document.getElementById("linkedin").innerText = resumeData.contact.linkedin;
+  document.querySelector(".highlighted-resume #name").innerText =
+    resumeData.name;
+  document.querySelector(".highlighted-resume #phone").innerText =
+    resumeData.contact.phone;
+  document.querySelector(".highlighted-resume #email").innerText =
+    resumeData.contact.email;
+  document.querySelector(".highlighted-resume #location").innerText =
+    resumeData.contact.location;
+  document.querySelector(".highlighted-resume #linkedin").innerText =
+    resumeData.contact.linkedin;
 
   // Populate Highlights
-  const highlightsContainer = document.getElementById("highlights");
+  const highlightsContainer = document.querySelector(
+    ".highlighted-resume #highlights ul"
+  );
   resumeData.highlights.forEach((highlight) => {
     const listItem = document.createElement("li");
     listItem.innerText = highlight;
@@ -26,37 +31,41 @@ function populateData(resumeData) {
   });
 
   // Populate Work Experience
-  const workExperienceContainer = document.getElementById("workExperience");
+  const workExperienceContainer = document.querySelector(
+    ".highlighted-resume #professional-experience"
+  );
   resumeData.workExperience.forEach((experience) => {
     const experienceDiv = document.createElement("div");
     experienceDiv.innerHTML = `
-        <h3>${experience.company} - ${experience.position}</h3>
-        <p>${experience.location} | ${experience.startDate} - ${
+          <h3>${experience.company} - ${experience.position}</h3>
+          <p>${experience.location} | ${experience.startDate} - ${
       experience.endDate
     }</p>
-        <ul>
-            ${experience.responsibilities
-              .map((responsibility) => `<li>${responsibility}</li>`)
-              .join("")}
-        </ul>
-    `;
+          <ul>
+              ${experience.responsibilities
+                .map((responsibility) => `<li>${responsibility}</li>`)
+                .join("")}
+          </ul>
+      `;
     workExperienceContainer.appendChild(experienceDiv);
   });
 
   // Populate Education
-  const educationContainer = document.getElementById("education");
+  const educationContainer = document.querySelector(
+    ".highlighted-resume #education"
+  );
   resumeData.education.forEach((education) => {
     const educationDiv = document.createElement("div");
     educationDiv.innerHTML = `
-        <h3>${education.university} - ${education.degree}</h3>
-        <p>${education.location} | ${education.startDate} - ${education.endDate}</p>
-    `;
+          <h3>${education.university} - ${education.degree}</h3>
+          <p>${education.location} | ${education.startDate} - ${education.endDate}</p>
+      `;
     educationContainer.appendChild(educationDiv);
   });
 
   // Handle References
   if (resumeData.referencesAvailable) {
-    document.getElementById("references").innerText =
+    document.querySelector(".highlighted-resume #reference h2").innerText =
       "References available upon request.";
   }
 }
