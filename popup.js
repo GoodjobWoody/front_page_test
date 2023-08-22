9151be27336053a8c6c43d4e773df7e4cf69f318
DoRightThing <rightwill@DoRightThings-MacBook-Pro.local>
2023-08-21 20:20:08 -0400

    modify popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,14 @@
 // Existing logic for opening the modal
+
+const API_ENDPOINTS = {
+  ADD_RESUME: "https://addresume-yuah7ub4ta-uc.a.run.app",
+  GET_RESUME: "https://getresume-yuah7ub4ta-uc.a.run.app",
+  CONVERT_DOC_TO_JSON: "https://convertdoctojson-yuah7ub4ta-uc.a.run.app",
+};
+
+const CLOUD_FUNCTION_TOKEN =
+  "eyJhbGciOiJSUzI1NiIsImtpZCI6ImMzYWZlN2E5YmRhNDZiYWU2ZWY5N2U0NmM5NWNkYTQ4OTEyZTU5NzkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTExODQ4MDc4MDM0OTk2NzczOTIwIiwiZW1haWwiOiJ3b29keS5jYXJlZXJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJtckVpVFhQMTlEdGxEeHF0ODBSMGJRIiwiaWF0IjoxNjkyNTgxNDc1LCJleHAiOjE2OTI1ODUwNzV9.icBD4Fk2vyl7AKVQX11Y3U3hS_UqYcjt1509kg_4shmNeD1qajWdrrf4SY9LW0c6sK7t1_VK17J6KB-pq0fil4oh1pzm1bs6oVEjDcmv7IMfoZ2fzn26H29CbLqqkssB4OgvEJ0L1YLLblH8PR123C-EdxvFJAY-U74fFCDvrPaY8IPvHnd3FyyGk4CVHIRqWMDlfB1W_0VjXUG0mkNmb-s8tWQs_idhHS_YLgR-4XzkZsPC9LJDcqMlsUiL3CTY_duh3QmKkpnlB79q_3YN5wuDNZcQBa6caTYfNS0G2lEHqKYgNFtVEwEHGkwc8hp_H5yuHeCA2BVM5sFbQsMBdA";
+
 document
   .getElementById("reviseResumeBtn")
   .addEventListener("click", function () {
@@ -62,6 +72,7 @@ function extractTextFromPDF(pdf) {
   Promise.all(allTextPromises).then(function (pagesText) {
     let resumeText = pagesText.join(" ");
     console.log(resumeText); // Log the extracted text to the console
+    const resumeJsonContent = convertDocToJson(resumeText);
   });
 }
 
@@ -95,4 +106,22 @@ function displayResult(result) {
 function handleError(err) {
   console.log(err);
   alert("Error parsing the document.");
+}
+
+function convertDocToJson(data) {
+  fetch(API_ENDPOINTS.CONVERT_DOC_TO_JSON, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + CLOUD_FUNCTION_TOKEN,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => {
+      console.error("Error sending data to API:", error);
+    });
 }
